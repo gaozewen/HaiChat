dae413a56613736b17a0317acc5688ed64f6f5af
gaozewen <1440651163@qq.com>
2018-12-19 23:11:00 +0800

    3-3完成第一个 Restful接口

--- Web/HaiChat/src/main/java/net/web/haichat/push/Application.java
+++ Web/HaiChat/src/main/java/net/web/haichat/push/Application.java
@@ -1,4 +1,19 @@
 package net.web.haichat.push;
 
-public class Application {
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.web.haichat.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
+public class Application extends ResourceConfig {
+
+    public Application() {
+        // 注册逻辑处理的包名
+        packages(AccountService.class.getPackage().getName());
+        // 注册 Json 解析器
+        register(JacksonJsonProvider.class);
+        // 注册日志打印输出
+        register(Logger.class);
+    }
 }
---
+++ Web/HaiChat/src/main/java/net/web/haichat/push/service/AccountService.java
@@ -0,0 +1,29 @@
+package net.web.haichat.push.service;
+
+import net.web.haichat.push.bean.User;
+
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+// 127.0.0.1/api/account/...
+@Path("/account")
+public class AccountService {
+
+    @GET
+    @Path("/login")
+    public String loginGet() {
+        return "You get the login.";
+    }
+
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User loginPost() {
+        User user = new User();
+        user.setName("美女");
+        user.setSex(2);
+        return user;
+    }
+}
