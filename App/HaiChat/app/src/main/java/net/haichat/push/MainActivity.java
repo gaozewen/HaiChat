349c20bd3a8735102b5b0a5d870627cc35b0c434
gaozewen <1440651163@qq.com>
2018-12-21 09:52:58 +0800

    4-5 优化 activity_main.xml 和 MainActivity.java 代码

--- App/HaiChat/app/src/main/java/net/haichat/push/MainActivity.java
+++ App/HaiChat/app/src/main/java/net/haichat/push/MainActivity.java
@@ -1,13 +1,39 @@
 package net.haichat.push;
 
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import net.haichat.common.app.Activity;
+import net.haichat.common.widget.PortraitView;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+    @BindView(R.id.appbar)
+    AppBarLayout mAppbar;       // 顶部导航条
+
+    @BindView(R.id.im_portrait)
+    PortraitView mPortrait;     // 头像控件
+
+    @BindView(R.id.txt_title)
+    TextView mTitle;            // 顶部导航条(标题)
+
+    @BindView(R.id.lay_container)
+    FrameLayout mContainer;     // 中间容器部分
+
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation; // 底部导航栏
 
     @Override
     protected int getContentLayoutId() {
@@ -17,5 +43,35 @@ public class MainActivity extends Activity {
     @Override
     protected void initWidget() {
         super.initWidget();
+
+        // 设置 appbar 背景图片
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop() // 居中剪切
+                .into(new ViewTarget<View, GlideDrawable>(mAppbar) {
+                    @Override
+                    public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                        // 这个 this 是 new ViewTarget
+                        // resource.getCurrent() 当前这一帧的图片
+                        this.view.setBackground(resource.getCurrent());
+                    }
+                });
     }
+    
+    /**
+     * 为 appbar 搜索按钮绑定点击事件
+     */
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick(){
+
+    }
+
+    /**
+     * 为右下角浮动的快捷操作按钮绑定单击事件
+     */
+    @OnClick(R.id.btn_action)
+    void onActionClick(){
+
+    }
+
 }
--- App/HaiChat/common/src/main/java/net/haichat/common/app/Activity.java
+++ App/HaiChat/common/src/main/java/net/haichat/common/app/Activity.java
@@ -1,9 +1,14 @@
 package net.haichat.common.app;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.List;
 
@@ -19,6 +24,15 @@ public abstract class Activity extends AppCompatActivity {
         initWindows();
 
         if(initArgs(getIntent().getExtras())) { // 初始化数据成功
+            // 状态栏透明
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+                Window window = getWindow();
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(Color.TRANSPARENT);
+            }
             // 得到界面 id 并设置到 界面中
             setContentView(getContentLayoutId());
             initWidget();
